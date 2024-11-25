4031c87701746c50902dbcc11f2af557e1129c31
is2mael <ismael.muh26@gmail.com>
2024-11-25 19:15:26 +0700

    Feat: Add Navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navbar />,
+  },
+]);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
