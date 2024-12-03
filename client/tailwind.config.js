72a110e66f460c05e8e11567d0c6008dcc814bdb
amala <amalananda332@gmail.com>
2024-12-03 12:16:37 +0700

    fix: added mark down & typography on gemini

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from 'daisyui';
+import daisyui from 'daisyui'
+import typography from '@tailwindcss/typography'
 
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
+  plugins: [daisyui, typography],
   daisyui: {
     themes: ["light", "dark"],
   },
-};
+}
